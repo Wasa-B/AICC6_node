b844d1fe0bc52f28254e70d671cecfbaae26dea5
Wasa-B <ghd1876@gmail.com>
2025-05-21 16:02:39 +0900

    getControllers

--- Apps/graph_app/back/controllers/getControllers.js
+++ Apps/graph_app/back/controllers/getControllers.js
@@ -1,100 +1,21 @@
 const database = require("../database/database");
 
-exports.getSalesMap = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM sales_map");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting sales map " + error.message,
-    });
+const getDataBase = (query, name)=>{
+  return async(req, res)=>{
+    try {
+      const result = await database.pool.query(query);
+      return res.status(200).json(result.rows);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error Occured while getting " + name + " " + error.message,
+      });
+    }
   }
 }
-
-exports.getVisitors = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM visitors");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting visitors " + error.message,
-    });
-  }
-}
-
-exports.getCostomers = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM customers");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting customers " + error.message,
-    });
-  }
-}
-
-exports.getTargetReality = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM target_reality");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting target reality " + error.message,
-    });
-  }
-}
-
-exports.getVolumeServices = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM volume_services");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting volume services " + error.message,
-    });
-  }
-}
-
-exports.getTopProducts = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM top_products");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting top products " + error.message,
-    });
-  }
-}
-
-exports.getRevenue = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM revenue");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting revenue " + error.message,
-    });
-  }
-}
-
-exports.getVisitors = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM visitors");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting visitors " + error.message,
-    });
-  }
-}
-
-exports.getVisitors = async(req,res)=>{
-  try {
-    const result = await database.pool.query("SELECT * FROM visitors");
-    return res.status(200).json(result.rows);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error Occured while getting visitors " + error.message,
-    });
-  }
-}
+exports.getSalesMap       = getDataBase("SELECT * FROM sales_map", "sales map");
+exports.getVisitors       = getDataBase("SELECT * FROM visitors", "visitors");
+exports.getCostomers      = getDataBase("SELECT * FROM customers", "customers");
+exports.getTargetReality  = getDataBase("SELECT * FROM target_reality", "target reality");
+exports.getVolumeServices = getDataBase("SELECT * FROM volume_services", "volume services");
+exports.getTopProducts    = getDataBase("SELECT * FROM top_products", "top products");
+exports.getRevenue        = getDataBase("SELECT * FROM revenue", "revenue");
