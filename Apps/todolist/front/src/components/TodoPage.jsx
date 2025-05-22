66ba43c251b7e9f7d86bc1989927d39c1073ae75
Wasa-B <ghd1876@gmail.com>
2025-05-22 09:35:50 +0900

    task view

--- Apps/todolist/front/src/components/TodoPage.jsx
+++ Apps/todolist/front/src/components/TodoPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskList from './TaskList'
 import { useSelector, useDispatch } from 'react-redux'
 import { addTask, completeTask, deleteTask } from '../app/toDoTask'
 import Task from './Task'
 const TodoPage = () => {
-  const tasks = useSelector((state) => state.tasks.value)
   const dispatch = useDispatch()
+  const tasks = useSelector((state) => state.tasks.value)
+  const [input, setInput] = useState("")
 
   const completeTaskAction = (id) => {
     return dispatch(completeTask(id))
@@ -14,15 +15,38 @@ const TodoPage = () => {
     return dispatch(deleteTask(id))
   }
 
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleAddTask()
+    }
+  }
+
+  const handleAddTask = () => {
+    if (input.trim() === "") return
+    dispatch(addTask({
+      title: input,
+      description: "New Description",
+      completed: false
+    }))
+    setInput("")
+  }
+
+
   return (
-    <div>
+    <div className='container px-5'>
       <h1>Todo Page</h1>
-      <button onClick={() => dispatch(addTask({
-        title: "New Task",
-        description: "New Description",
-        completed: false
-      }))}>Add Task</button>
-      <TaskList tasks={tasks} template={Task} completeTask={completeTaskAction} deleteTask={deleteTaskAction} />
+      <div>
+        <input type="text" placeholder='Add a new task' value={input} onKeyDown={handleKeyDown} onChange={handleInputChange} />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <div className=''>
+        <TaskList tasks={tasks} template={Task} completeTask={completeTaskAction} deleteTask={deleteTaskAction} />
+      </div>
     </div>
   )
 }
